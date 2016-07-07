5e7b1da439f83ed9160213d300de400eb307d665
Jacob Vogelbacher <jacob_vogelbacher@knights.ucf.edu>
2016-07-07 18:42:13 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,3 @@
 <?php
-	/*$mysqli = new mysqli("sulley.cah.ucf.edu", "ja941580", "beastmode", "ja941580");*/
-
-	$mysqli = new mysqli("localhost", "ja941580", "beastmode", "ja941580");
+	$mysqli = new mysqli("localhost", "username", "password", "database");
 ?>
